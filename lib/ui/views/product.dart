1312135c621c6d92b1b30eb97657e4286556dac3
Hugo Bezerra Pimentel <hugobezerrapimentel@gmail.com>
2021-06-02 18:37:16 -0300

    [CHANGE] Refatoração da view Product adicionando extend para o controller HomeController.

--- lib/ui/views/product.dart
+++ lib/ui/views/product.dart
@@ -5,14 +5,14 @@ import 'package:proj_ctrl/constants/args.dart';
 import 'package:proj_ctrl/controllers/home.dart';
 import 'package:proj_ctrl/ui/components/products/product_input.dart';
 
-class ProductView extends StatelessWidget {
+class ProductView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Get.arguments == Arguments.argIsProductInput
           ? ProductInputComponent(
-              productHelper: HomeController.to.productHelper,
+              productHelper: controller.productHelper,
             )
           : SizedBox(),
     );
